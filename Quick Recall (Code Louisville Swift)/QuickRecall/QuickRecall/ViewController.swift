8e4fbec5ac2b8c8b1cfc3b10b4331aa91058b6ef
Nathaniel Hatfield <Alyssia@Alyssias-MacBook-Air.local>
2017-05-31 18:27:29 -0400

    Testing voice recognition w/ Speech Kit
    Using tutorial found here:
    
    http://www.appcoda.com/siri-speech-framework/

--- Quick Recall (Code Louisville Swift)/QuickRecall/QuickRecall/ViewController.swift
+++ Quick Recall (Code Louisville Swift)/QuickRecall/QuickRecall/ViewController.swift
@@ -7,23 +7,55 @@
 //
 
 import UIKit
+import Speech
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SFSpeechRecognizerDelegate {
 
     @IBOutlet weak var questionLabel: UILabel!
     
     @IBOutlet weak var answerButton: UIButton!
     
+    @IBOutlet weak var microphoneButton: UIButton!
+  
     let questionProvider = QuestionProvider()
     var currentQuestion: Question = Question("", [""])
     var questionNumber: Int = 1
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
         currentQuestion = questionProvider.randomQuestion()
         questionLabel.text = "Question \(questionNumber): \(currentQuestion.clue)"
         
+        microphoneButton.isEnabled = false
+        speechRecognizer.delegate = self
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in
+            
+            var isButtonEnabled = false
+            
+            switch authStatus {
+            case .authorized:
+                isButtonEnabled = true
+                
+            case .denied:
+                isButtonEnabled = false
+                print("User denied access to speech recognition")
+                
+            case .restricted:
+                isButtonEnabled = false
+                print("Speech recognition restricted on this device")
+                
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Speech recognition not yet authorized")
+            }
+            
+            OperationQueue.main.addOperation() {
+                self.microphoneButton.isEnabled = isButtonEnabled
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,6 +69,10 @@ class ViewController: UIViewController {
         questionNumber += 1
         questionLabel.text = "Question \(questionNumber): \(currentQuestion.clue)"
         
+    }
+    
+    @IBAction func microphoneTapped(_ sender: AnyObject) {
+    
     }
 
 }
