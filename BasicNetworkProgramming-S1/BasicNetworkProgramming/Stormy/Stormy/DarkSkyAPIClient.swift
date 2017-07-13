1185f38c983ae432c10cdb5b38fa8a3dcd32e647
Nathaniel Hatfield <Alyssia@Alyssias-MacBook-Air.local>
2017-07-13 18:49:46 -0400

    Refactoring per Treehouse videos

---
+++ BasicNetworkProgramming-S1/BasicNetworkProgramming/Stormy/Stormy/DarkSkyAPIClient.swift
@@ -0,0 +1,14 @@
+//
+//  DarkSkyAPIClient.swift
+//  Stormy
+//
+//  Created by Alyssia Higgs on 7/13/17.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+class DarkSkyAPIClient {
+    fileprivate let darkSkyApiKey = "adf6d57e36957e779d7db8b6585efef9"
+
+}
---
+++ BasicNetworkProgramming-S1/BasicNetworkProgramming/Stormy/Stormy/DarkSkyError.swift
@@ -0,0 +1,16 @@
+//
+//  DarkSkyError.swift
+//  Stormy
+//
+//  Created by Alyssia Higgs on 7/13/17.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+enum DarkSkyError: Error {
+    case requestFailed
+    case responseUnsuccessful
+    case invalidData
+    case jsonConversionFailure
+}
